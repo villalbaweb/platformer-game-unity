39ac7edb8a843885cf226d1bca69df2794deb0e8
villalbaweb <villalbaweb@gmail.com>
2019-12-04 18:29:53 -0600

    First Draft

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    // Config params
+    [Header("Health")]
+    [SerializeField] int health = 300;
+
+    [Header("Audio Effects")]
+    [SerializeField] AudioClip dieAudioSFX;
+
+    // Cache
+    Animator _animator;
+    bool isDead;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        isDead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckLife();   
+    }
+
+    private void CheckLife()
+    {
+        if(!isDead && health <= 0)
+        {
+            StartCoroutine(DieVFX());
+        }
+    }
+
+    IEnumerator DieVFX()
+    {
+        isDead = true;
+        _animator.SetTrigger("Die");
+        PlayDieSFX();
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
+
+    private void PlayDieSFX()
+    {
+        if (!dieAudioSFX) { return; }
+
+        AudioSource.PlayClipAtPoint(dieAudioSFX, Camera.main.transform.position);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
---
+++ Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenePersist : MonoBehaviour
+{
+    // Cache
+    LevelLoader _levelLoader;
+    int startSceneIndex;
+
+    private void Awake()
+    {
+        int numScenePersistObjects = FindObjectsOfType<ScenePersist>().Length;
+
+        if (numScenePersistObjects > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _levelLoader = FindObjectOfType<LevelLoader>();
+        startSceneIndex = _levelLoader.GetCurrentSceneIndex();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int currentSceneIndex = _levelLoader.GetCurrentSceneIndex();
+        if(currentSceneIndex != startSceneIndex)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
